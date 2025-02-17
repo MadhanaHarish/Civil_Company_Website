f20ba70e756f5e1590b9af411e45021824948371
madhan <rajagopalamadhan@gmail.com>
2025-02-17 20:44:38 +0530

    Created projects design overall

--- Site/src/components/Projects.jsx
+++ Site/src/components/Projects.jsx
@@ -1,15 +1,58 @@
 import React from "react";
+import Card from "./Card.jsx";
 
 const Projects = () => {
     return (
         <>
             <div className="mt-20">
                 <div className="text-center">
-                    <h1 className="text-4xl font-bold mb-4">Projects</h1>
+                    <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4 relative">
+                        <span className="w-full sm:w-auto relative">
+                            <input
+                                type="text"
+                                className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pr-12 pl-4 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                                placeholder="Germany..."
+                            />
+                            <button
+                                className="absolute right-1 top-1 rounded bg-slate-800 p-1.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                type="button"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    className="w-4 h-4"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </span>
+                        <div className="flex space-x-4">
+                            <button
+                                className="p-2.5 m-2 text-sm font-medium text-white bg-gray-700 rounded-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                            >
+                                Filters
+                            </button>
+                            <button
+                                className="p-2.5 m-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                            >
+                                Add Project
+                            </button>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                {[...Array(10)].map((_, index) => (
+                    <Card key={index}/>
+                ))}
             </div>
         </>
     );
-}
+};
 
 export default Projects;
